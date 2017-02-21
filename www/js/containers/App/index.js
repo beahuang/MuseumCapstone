a28a715202eee413f5a4bb1c2113a0578ff95d37
Beatrice Huang <beahuang1@gmail.com>
2017-02-21 16:21:57 -0500

    move screens folder to containers

--- www/js/containers/App/index.js
+++ www/js/containers/App/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-import TourScreen from '../../screens/TourScreen';
-import TimelineScreen from '../../screens/TimelineScreen';
-import MapScreen from '../../screens/MapScreen';
-import BrowseScreen from '../../screens/BrowseScreen';
+import TourScreen from '../TourScreen';
+import TimelineScreen from '../TimelineScreen';
+import MapScreen from '../MapScreen';
+import BrowseScreen from '../BrowseScreen';
 
 export default class App extends Component {
   render() {
