2f6d53e00edce701559be36a8dbf067e01789770
Beatrice Huang <beahuang1@gmail.com>
2017-02-21 15:47:31 -0500

    add in basic navigation

--- www/js/components/Navigation/index.js
+++ www/js/components/Navigation/index.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import { Link } from 'react-router';
 
+import CompassIcon from '../../icons/Compass';
+import TimelineIcon from '../../icons/Timeline';
+import MapIcon from '../../icons/Map';
+import BrowseIcon from '../../icons/Browse';
+
 export default function Navigation() {
   return (
     <nav>
       <ul>
-        <li><Link to='/' activeClassName='active'>Tours</Link></li>
-        <li><Link to='/timeline' activeClassName='active'>Timeline</Link></li>
-        <li><Link to='/map' activeClassName='active'>Map</Link></li>
-        <li><Link to='/browse' activeClassName='active'>Browse</Link></li>
+        <li><Link to='/' activeClassName='active'><CompassIcon/>Tours</Link></li>
+        <li><Link to='/timeline' activeClassName='active'><TimelineIcon/>Timeline</Link></li>
+        <li><Link to='/map' activeClassName='active'><MapIcon/>Map</Link></li>
+        <li><Link to='/browse' activeClassName='active'><BrowseIcon/>Browse</Link></li>
       </ul>
     </nav>
   )
