5e61bf01de83e2e0828084c50d7dec57ec706b65
Beatrice Huang <beahuang1@gmail.com>
2017-02-21 20:55:02 -0500

    remove screens folder and remove screen components to components folder

--- www/js/containers/App/index.js
+++ www/js/containers/App/index.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import { Router, Route, Link, IndexRoute, hashHistory, browserHistory } from 'react-router';
 
-import TourScreen from '../../screens/TourScreen';
-import TimelineScreen from '../../screens/TimelineScreen';
-import MapScreen from '../../screens/MapScreen';
-import BrowseScreen from '../../screens/BrowseScreen';
+import TourScreen from '../../components/TourScreen';
+import TimelineScreen from '../../components/TimelineScreen';
+import MapScreen from '../../components/MapScreen';
+import BrowseScreen from '../../components/BrowseScreen';
 
 export default class App extends Component {
   render() {
