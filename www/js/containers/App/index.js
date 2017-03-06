d2cf4d22fd6b364e9288c10ec81af6d43c8a7b71
Alice Young <aliceyoung.9@gmail.com>
2017-03-06 02:31:23 -0500

    init firebase

--- www/js/containers/App/index.js
+++ www/js/containers/App/index.js
@@ -1,6 +1,16 @@
 import React, { Component } from 'react';
+import * as firebase from 'firebase';
 
 import Navigation from '../../components/Navigation';
+
+const config = {
+  apiKey: process.env.API_KEY,
+  authorDomain: process.env.AUTH_DOMAIN,
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET,
+  messagingSenderId: process.env.MESSAGING_SENDER_ID
+}
+firebase.initializeApp(config);
 
 export default class App extends Component {
   render() {
