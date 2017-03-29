90336bec0249e4b80eeb19b5399044bffb1377e0
Alice Young <aliceyoung.9@gmail.com>
2017-03-28 22:29:14 -0400

    Revert to HashRouter so I can refresh

--- www/js/components/Router/index.js
+++ www/js/components/Router/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 
 import Navigation from '../../components/Navigation';
 
