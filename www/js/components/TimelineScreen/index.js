19d2f331555018baa2d4cb5ae642cb21df51252a
Alice Young <aliceyoung.9@gmail.com>
2017-03-30 01:19:34 -0400

    Refactor: Start propifying and splitting pieces.

--- www/js/components/TimelineScreen/index.js
+++ www/js/components/TimelineScreen/index.js
@@ -2,32 +2,43 @@ import React, { Component } from 'react';
 
 import CompassIcon from '../../icons/Compass';
 
-function JourneyItem(props) {
+function TourHeader(props) {
   return(
-    <div className='journey-item'>
-      <div className='journey-item-left'>
-        <h2 className='journey-item-title'>Title Here.</h2>
+    <div className='tour-item'>
+      <div className='tour-item-left'>
+        <h2 className='tour-item-title'>{props.title}</h2>
 
-        <div className='journey-item-row'>
-          <div className='journey-item-icon'>
+        <div className='tour-item-row'>
+          <div className='tour-item-icon'>
             <CompassIcon/>
           </div>
 
-          <h3 className='journey-item-desc'>type</h3>
+          <h3 className='tour-item-desc'>Tour</h3>
           <hr/>
-          <div className='journey-item-times'>
+          <div className='tour-item-times'>
             3:40pm
           </div>
         </div>
 
-        <p className='journey-item-piece-count'>
+        <p className='tour-item-piece-count'>
           30 minutes<br/>
-          20 pieces
+          {props.pieceCount} pieces
         </p>
       </div>
-      <div className='journey-item-img'>
+      <div className='tour-item-img'>
         <img src='img/featured.png'/>
       </div>
+    </div>
+  )
+}
+
+function Tour(props) {
+  return(
+    <div className='tour'>
+      <h4 className='tour-date'>Your Journey from February 14<sup>th</sup>, 2017</h4>
+      <TourHeader
+        title={props.title}
+        pieceCount={props.pieceCount}/>
     </div>
   )
 }
@@ -35,19 +46,16 @@ function JourneyItem(props) {
 export default class TimelineScreen extends Component {
   constructor() {
     super();
-    this.state = {
-      title: 'Timeline Screen',
-    };
+    this.state = {};
   }
 
   render() {
     return (
       <div className='timeline'>
         <h3 className='timeline-title'>My Timeline</h3>
-        <div className='journey'>
-          <h4 className='journey-date'>Your Journey from February 14<sup>th</sup>, 2017</h4>
-          <JourneyItem/>
-        </div>
+        <Tour
+          title='Tour Title'
+          pieceCount='20'/>
       </div>
     );
   }
