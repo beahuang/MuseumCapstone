6919ed09e81e578d3b5f76cf65a37b95f7cbb9bb
Alice Young <aliceyoung.9@gmail.com>
2017-04-04 19:17:17 -0400

    Piece page: Start with Header, with Piece image, title, and button row.

--- www/js/components/PieceScreen/index.js
+++ www/js/components/PieceScreen/index.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+
+import CameraIcon from '../../icons/Camera';
 
 class PieceScreen extends Component {
+  _buttonRow() {
+    return (
+      <div className='piece-sc-button-row'>
+        <button><CameraIcon/></button>
+      </div>
+    )
+  }
+
 
   render() {
+    const { props: { piece } } = this
+
+    const width = window.innerWidth * 2;
+
     return (
       <div className='piece-screen'>
-        { this.props.piece.title }
+        <div
+          className='piece-sc-header'
+          style={ piece.primaryimageurl ?
+            { 'backgroundImage': `url(${piece.primaryimageurl}?height=800&width=${width})` } :
+            { 'backgroundColor': '#666' } }
+        >
+          <div className='piece-sc-header-content'>
+            <h1>{ piece.title }</h1>
+            <h4> { piece.people ? `${piece.people[0].name}, ` : ''} { piece.dated } </h4>
+            {this._buttonRow()}
+          </div>
+        </div>
       </div>
     )
   }
